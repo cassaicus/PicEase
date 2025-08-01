490a4d07e8f762ee07f647783c749ba9d2c2094b
cassaicus <cassaicus+x@gmail.com>
2025-08-01 17:10:32 +0900

    サムネイル浮上

--- PicEase/ContentView.swift
+++ PicEase/ContentView.swift
@@ -2,16 +2,33 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var controller = PageControllerWrapper()
+    @State private var isThumbnailVisible = true
 
     var body: some View {
         VStack(spacing: 0) {
             PageControllerRepresentable(controller: controller)
                 .edgesIgnoringSafeArea(.all)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .onHover { hovering in
+                    if hovering {
+                        withAnimation {
+                            isThumbnailVisible = true
+                        }
+                    } else {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            withAnimation {
+                                isThumbnailVisible = false
+                            }
+                        }
+                    }
+                }
 
-            ThumbnailScrollView(imageURLs: controller.imagePaths, currentIndex: $controller.selectedIndex)
-                .frame(height: 100)
-                .background(Color.black.opacity(0.8))
+            if isThumbnailVisible {
+                ThumbnailScrollView(imageURLs: controller.imagePaths, currentIndex: $controller.selectedIndex)
+                    .frame(height: 100)
+                    .background(Color.black.opacity(0.8))
+                    .transition(.move(edge: .bottom).combined(with: .opacity))
+            }
         }
     }
 }
