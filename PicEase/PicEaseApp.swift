e7a2b3dacff6a4012bbc1e35c3ea2c371e164022
cassaicus <cassaicus+x@gmail.com>
2025-08-01 15:24:29 +0900

    Initial Commit

---
+++ PicEase/PicEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicEaseApp.swift
+//  PicEase
+//
+//  Created by ibis on 2025/08/01.
+//
+
+import SwiftUI
+
+@main
+struct PicEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
